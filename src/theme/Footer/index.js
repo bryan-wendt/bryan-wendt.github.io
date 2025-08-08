9aeb6e7ed0e73360517fbed7fb38a37e511dcd07
Bryan Wendt <bpwendt0@gmail.com>
2025-08-08 15:56:03 -0400

    Update new footer

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,11 +1,61 @@
 import React from 'react';
 import Footer from '@theme-original/Footer';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import Heading from '@theme/Heading';
 
-export default function FooterWrapper(props) {
+import styles from './styles.module.css';
+
+const footerLinks = {
+  style: 'dark',
+  links: [
+    {
+      title: 'Projects',
+      items: [
+        {
+          label: 'Physical Red Team Wiki',
+          href: '/docs/intro',
+        },
+        {
+          label: 'Twitter',
+          href: 'https://twitter.com/docusaurus',
+        },
+      ],
+    },
+    {
+      title: 'Community',
+      items: [
+        {
+          label: 'Discord',
+          href: 'https://discord.com/users/bw#6573',
+        },
+        {
+          label: 'X',
+          href: 'https://x.com/bryan_wendt',
+        },
+      ],
+    },
+    {
+      title: 'More',
+      items: [
+        {
+          label: 'Blog',
+          to: '/blog',
+        },
+        {
+          label: 'GitHub',
+          href: 'https://github.com/bryan-wendt',
+        },
+      ],
+    },
+  ],
+  copyright: `Copyright © ${new Date().getFullYear()} Bryan Wendt. Built with Docusaurus.`,
+};
+
+function FooterWrapper(props) {
   return (
-    <div className={clsx('col col--6')}>
-      <div className="text--center"></div>
-        <Footer {...props} />
+    <div className={clsx(styles.footerWrapper)}>
+      <Footer {...props} {...footerLinks} />
     </div>
   );
 }
