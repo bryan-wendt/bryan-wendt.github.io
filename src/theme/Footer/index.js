5178d001e7d24cfbe532d5f25ddc2c0330c175fb
Bryan Wendt <bpwendt0@gmail.com>
2025-08-08 16:00:48 -0400

    fixed errors

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -52,7 +52,7 @@ const footerLinks = {
   copyright: `Copyright © ${new Date().getFullYear()} Bryan Wendt. Built with Docusaurus.`,
 };
 
-function FooterWrapper(props) {
+function Footer(props) {
   return (
     <div className={clsx(styles.footerWrapper)}>
       <Footer {...props} {...footerLinks} />
